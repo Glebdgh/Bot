927068ee4d068513c87873d2fe219613ed636472
Gleb <gleb.putintsev@icloud.com>
2021-07-31 14:14:46 +0300

    Modified Bash script

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
- sudo apt-get remove docker docker-engine docker.io containerd runc
+ sudo apt-get remove -y docker docker-engine docker.io containerd runc
  
- sudo apt-get update
+ sudo apt-get update -y
  
- sudo apt-get install \
+ sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -17,6 +17,6 @@
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
- sudo apt-get update
+ sudo apt-get update -y
 
- sudo apt-get install docker-ce docker-ce-cli containerd.io
+ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
